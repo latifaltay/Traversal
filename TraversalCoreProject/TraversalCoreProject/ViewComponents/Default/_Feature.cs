bd9ca74465d0ff65ea78bb3aae4bd9038a3dc727
Latif Altay <latifaltay00@gmail.com>
2024-05-05 19:55:18 +0300

    Test

---
+++ TraversalCoreProject/TraversalCoreProject/ViewComponents/Default/_Feature.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.Default
+{
+    public class _Feature : ViewComponent
+    {
+        FeatureManager FeatureManager = new FeatureManager(new EfFeatureDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = FeatureManager.GetAll();
+            return View(values);
+        }
+    }
+}
